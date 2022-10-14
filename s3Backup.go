f73f6dd20988101ddf4c27611cbeffa79f2b481f
Trinopoty Biswas <trinopoty@hotmail.com>
2022-10-15 00:18:38 +0200

    Implemented s3 backup

---
+++ s3Backup.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"io"
+	"log"
+	"math"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+)
+
+type ProgressTrackingReader struct {
+	io.ReadCloser
+
+	currentPosition int64
+	lastRead        int
+	Report          func(totalRead int64)
+}
+
+func (pr *ProgressTrackingReader) Read(buff []byte) (int, error) {
+	pr.currentPosition += int64(pr.lastRead)
+	pr.lastRead = 0
+	pr.Report(pr.currentPosition)
+
+	readCount, err := pr.ReadCloser.Read(buff)
+	if err == nil {
+		pr.lastRead = readCount
+	}
+	return readCount, err
+}
+
+func (pr *ProgressTrackingReader) Close() error {
+	pr.currentPosition += int64(pr.lastRead)
+	pr.lastRead = 0
+	pr.Report(pr.currentPosition)
+
+	return pr.ReadCloser.Close()
+}
+
+func NewProgressTrackingReader(file *os.File, report func(read int64)) *ProgressTrackingReader {
+	return &ProgressTrackingReader{file, 0, 0, report}
+}
+
+func main() {
+	awsProfilePtr := flag.String("profile", "", "AWS profile (optional)")
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var cfgOptions []func(*config.LoadOptions) error
+	if awsProfilePtr != nil && len(*awsProfilePtr) > 0 {
+		cfgOptions = []func(*config.LoadOptions) error{
+			config.WithSharedConfigProfile(*awsProfilePtr),
+		}
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), cfgOptions...)
+	if err != nil {
+		log.Fatal("Unable to create AWS config")
+	}
+
+	s3Client := s3.NewFromConfig(cfg)
+	doBackup(s3Client, flag.Arg(0), flag.Arg(1))
+}
+
+func doBackup(s3Client *s3.Client, source string, destination string) {
+	if !strings.HasPrefix(destination, "s3://") {
+		fmt.Println("Backup destination is not s3")
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(source); errors.Is(err, os.ErrNotExist) {
+		fmt.Println("Backup source does not exist")
+		os.Exit(1)
+	}
+
+	s3Url, err := url.Parse(destination)
+	if err != nil {
+		fmt.Println("Unable to parse backup destination path")
+		os.Exit(1)
+	}
+
+	if len(s3Url.Host) <= 0 {
+		fmt.Println("Unable to parse backup destination path")
+		os.Exit(1)
+	}
+
+	s3Key := s3Url.Path
+
+	// If path ends with '/', target is inside the provided path, otherwise,
+	// target is the path itself
+	srcFile := source
+
+	s3IsDir := strings.HasSuffix(s3Key, "/")
+	srcIsDir := strings.HasSuffix(srcFile, "/")
+
+	if s3IsDir {
+		if srcIsDir {
+			s3Key = s3Key[:len(s3Key)-1]
+			srcFile = srcFile[:len(srcFile)-1]
+		} else {
+			s3Key = s3Key + path.Base(srcFile)
+		}
+	} else {
+		if srcIsDir {
+			fmt.Printf("Cannot copy contents of directory %s to s3 path %s\n", srcFile, s3Key)
+			os.Exit(1)
+		}
+	}
+
+	Upload(s3Client, manager.NewUploader(s3Client), s3Url.Host, s3Key, srcFile)
+}
+
+func Upload(s3Client *s3.Client, s3Uploader *manager.Uploader, bucketName string, s3Key string, srcFile string) {
+	if strings.HasPrefix(s3Key, "/") {
+		s3Key = s3Key[1:]
+	}
+
+	if info, err := os.Stat(srcFile); err == nil {
+		if info.IsDir() {
+			if files, err := os.ReadDir(srcFile); err == nil {
+				for _, f := range files {
+					f := f.Name()
+					Upload(s3Client, s3Uploader, bucketName, s3Key+"/"+f, path.Join(srcFile, f))
+				}
+			} else {
+				fmt.Printf("Unable to get files in directory %s\n", srcFile)
+			}
+		} else {
+			fmt.Printf("Uploading %s to %s\n", srcFile, s3Key)
+
+			if f, err := os.Open(srcFile); err == nil {
+				defer f.Close()
+
+				hash := sha256.New()
+				if _, err := io.Copy(hash, f); err == nil {
+					modTime := info.ModTime().Format("2006-01-02 15:04:05")
+					hash := fmt.Sprintf("%x", hash.Sum(nil))
+
+					exists := false
+					if response, err := s3Client.HeadObject(context.TODO(), &s3.HeadObjectInput{
+						Bucket: aws.String(bucketName),
+						Key:    aws.String(s3Key),
+					}); err == nil {
+						if !response.DeleteMarker {
+							if val, ok := response.Metadata["modified-timestamp"]; ok {
+								exists = exists || (val == modTime)
+							}
+							if val, ok := response.Metadata["sha256"]; ok {
+								exists = exists || (val == hash)
+							}
+						}
+					}
+
+					if !exists {
+						f.Seek(0, io.SeekStart)
+
+						metadata := make(map[string]string)
+						metadata["modified-timestamp"] = modTime
+						metadata["sha256"] = hash
+
+						s3Body := NewProgressTrackingReader(f, func(totalRead int64) {
+							percentDone := int(math.Floor(float64(100*totalRead) / float64(info.Size())))
+							fmt.Printf("\r[%s%s] %d %% (%s/%s)",
+								strings.Repeat("=", percentDone),
+								strings.Repeat(" ", 100-percentDone),
+								percentDone,
+								FormatSize(totalRead),
+								FormatSize(info.Size()))
+						})
+
+						if _, err := s3Uploader.Upload(context.TODO(), &s3.PutObjectInput{
+							Bucket:   aws.String(bucketName),
+							Key:      aws.String(s3Key),
+							Body:     s3Body,
+							Metadata: metadata,
+						}); err != nil {
+
+						}
+
+						fmt.Printf("\n")
+					} else {
+						fmt.Printf("%s already exists. Skipping...\n", s3Key)
+					}
+				}
+			}
+		}
+	}
+}
+
+func FormatSize(size int64) string {
+	suffixes := []string{"B", "KB", "MB", "GB", "TB"}
+	idx := 0
+
+	for true {
+		if size < 1024 || (idx+1) >= len(suffixes) {
+			return fmt.Sprintf("%d %s", size, suffixes[idx])
+		} else {
+			size /= 1024
+			idx += 1
+		}
+	}
+
+	return ""
+}
